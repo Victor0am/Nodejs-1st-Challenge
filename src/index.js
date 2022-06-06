0d78ab3e6f7eb5c2e77b558d8c017c5f9146cc83
Victor Aguiar <victoramarques@hotmail.com>
2022-06-06 16:13:11 -0300

    Create user route

--- src/index.js
+++ src/index.js
@@ -1,21 +1,53 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
+
+// Rota de cadastro de usuário
+/**
+ * Cadastro de usuário no sistema
+ * 
+ * name - nome do usuário
+ * username - "nick" de usuário
+ * 
+ * 201 -> Retorna um usuário com id, name, username, e lista de afazeres vazia
+ * 
+ * 400 -> Erro - Usuário já existe
+ */
+
 app.post('/users', (request, response) => {
   // Complete aqui
+  const {name, username} = request.body;
+
+  const userExists = users.find(u => u.username === username);
+
+  if (userExists) {
+    return response.status(400).json({error: 'User already exists.'});
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.status(201).json(user);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
